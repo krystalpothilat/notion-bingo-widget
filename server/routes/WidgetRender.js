fdf803a9cbb64e6fbe9af7d1f41e91c1b42012b6
krystalpothilat <krystalpothilat@gmail.com>
2024-02-26 18:48:05 -0800

    change origin for widgetrender

--- server/routes/WidgetRender.js
+++ server/routes/WidgetRender.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const Widget = require('../models/Widget');
 
 router.use(cors({
-  origin: 'https://notion-bingo-widget-server.vercel.app',
+  origin: 'https://notion-bingo-widget.vercel.app',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
