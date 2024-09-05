f69febc5817952ba5be8f7da0727449566c3351c
krystalpothilat <krystalpothilat@gmail.com>
2024-09-04 17:08:21 -0700

    update headers

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 
 // Remove COOP and CORP headers
 app.use((req, res, next) => {
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    // res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    // res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 
     res.cookie('cookieName', 'cookieValue', {
         sameSite: 'None', 
